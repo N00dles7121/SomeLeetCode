86401bd344bd8d3b6e59de9208b12a533ff209bd
Albert Kargaev Laptop <n00dles7121@gmail.com>
2025-01-03 11:57:23 -0500

    MajorityElement solution

--- MajorityElement.cs
+++ MajorityElement.cs
@@ -7,6 +7,35 @@ namespace LeetCode
 {
     public class MajorityElement
     {
-        
+        // Given an array nums of size n, return the majority element.
+        // The majority element is the element that appears more than ⌊n / 2⌋ times. 
+        // You may assume that the majority element always exists in the array.
+
+        public int Solution(int[] nums)
+        {
+            int num = 0;
+            int counter = 0;
+
+            foreach (var n in nums)
+            {
+                // Since we don't need to count number of occurances of majority element
+                // but need just value of the element we can use variable "counter" to keep 
+                // track of which value is most frequet.
+
+                if (counter == 0)
+                {
+                    num = n;
+                }
+                if (num == n)
+                {
+                    counter++;
+                }
+                else
+                {
+                    counter--;
+                }
+            }
+            return num;
+        }
     }
 }
