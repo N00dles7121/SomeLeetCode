142acc4e4faab4ef498552dddd76e4f995bce32d
Albert Kargaev Laptop <n00dles7121@gmail.com>
2025-01-04 20:52:35 -0500

    Class made static

--- BuySellStock.cs
+++ BuySellStock.cs
@@ -5,13 +5,13 @@ using System.Threading.Tasks;
 
 namespace LeetCode
 {
-    public class BuySellStock
+    public static class BuySellStock
     {
         // You are given an array prices where prices[i] is the price of a given stock 
         // on the ith day. You want to maximize your profit by choosing a single day 
         // to buy one stock and choosing a different day in the future to sell that stock.
 
-        public int Solution(int[] prices)
+        public static int Solution(int[] prices)
         {
             int profit = 0;
             int buyPrice = prices[0];
