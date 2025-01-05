25f30ada298cdecc97a5d1951979f39821f5f46a
Albert Kargaev Laptop <n00dles7121@gmail.com>
2025-01-04 20:52:59 -0500

    all solutions added to Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using LeetCode;
+
+BuySellStock.Solution(new int[] { 1, 3, 5, 6, 2, 3, 7, 9 });
+
+CommonPrefix.Solution(new string[] { "flower", "flour", "flavour", "flight" });
+
+LenLastWord.Solution("Some string to test how this works");
+
+MajorityElement.Solution(new int[] { 1, 2, 3, 1, 1, 1, 5, 6, 7, 3, 3, 3, 3, 3 });
+
+MergeSortedArray.Solution(new int[] { 1, 2, 3, 0, 0, 0 }, 3, new int[] { 2, 5, 6 }, 3);
+
+NeedleHaystack.Solution2("leeto", "leetcode");
+
+PalindromeNumber.Solution(1234554321);
+
+RemoveDuplicates.Solution(new int[] { 1, 1, 1, 2, 2, 3, 4, 5 });
+
+RemoveElement.Solution(new int[] { 1, 1, 1, 2, 2, 3, 4, 5 }, 1);
+
+TwoSum.Solution(new int[] { 1, 1, 1, 2, 2, 3, 4, 5 }, 6);
