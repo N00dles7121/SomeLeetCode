ed0dd7b03e4bbfb41ffbd34251d0470a03be766a
Albert Kargaev Laptop <n00dles7121@gmail.com>
2025-01-09 17:12:17 -0500

    LemonadeChange and CountWordsWprefix added to Program.cs

--- Program.cs
+++ Program.cs
@@ -1,21 +1,25 @@
 ﻿using LeetCode;
 
-BuySellStock.Solution(new int[] { 1, 3, 5, 6, 2, 3, 7, 9 });
+BuySellStock.Solution([1, 3, 5, 6, 2, 3, 7, 9]);
 
-CommonPrefix.Solution(new string[] { "flower", "flour", "flavour", "flight" });
+CommonPrefix.Solution(["flower", "flour", "flavour", "flight"]);
 
 LenLastWord.Solution("Some string to test how this works");
 
-MajorityElement.Solution(new int[] { 1, 2, 3, 1, 1, 1, 5, 6, 7, 3, 3, 3, 3, 3 });
+MajorityElement.Solution([1, 2, 3, 1, 1, 1, 5, 6, 7, 3, 3, 3, 3, 3]);
 
-MergeSortedArray.Solution(new int[] { 1, 2, 3, 0, 0, 0 }, 3, new int[] { 2, 5, 6 }, 3);
+MergeSortedArray.Solution([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
 
 NeedleHaystack.Solution2("leeto", "leetcode");
 
 PalindromeNumber.Solution(1234554321);
 
-RemoveDuplicates.Solution(new int[] { 1, 1, 1, 2, 2, 3, 4, 5 });
+RemoveDuplicates.Solution([1, 1, 1, 2, 2, 3, 4, 5]);
 
-RemoveElement.Solution(new int[] { 1, 1, 1, 2, 2, 3, 4, 5 }, 1);
+RemoveElement.Solution([1, 1, 1, 2, 2, 3, 4, 5], 1);
 
-TwoSum.Solution(new int[] { 1, 1, 1, 2, 2, 3, 4, 5 }, 6);
+TwoSum.Solution([1, 1, 1, 2, 2, 3, 4, 5], 6);
+
+CountWordsWPrefix.Solution(["pay", "attention", "practice", "attend"], "at");
+
+LemonadeChange.Solution([5, 5, 5, 10, 20, 10, 5, 5, 10, 20, 20]);
